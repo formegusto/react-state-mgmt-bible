cf293983c56a20e71954a4daa414f130ccf91fb4
formegusto <formegusto@gmail.com>
2022-04-28 15:04:42 +0900

    recoil update : Hello Recoil !

---
+++ atomic/recoil-bible/src/App.tsx
@@ -0,0 +1,7 @@
+import HelloRecoil from "./components/HelloRecoil";
+
+function App() {
+  return <HelloRecoil />;
+}
+
+export default App;
---
+++ atomic/recoil-bible/src/components/HelloRecoil.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
+const textState = atom({
+  key: "textState",
+  default: "",
+});
+
+const charCountState = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setText(e.target.value);
+    },
+    [setText]
+  );
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function HelloRecoil() {
+  return (
+    <>
+      <TextInput />
+      <CharacterCount />
+    </>
+  );
+}
+
+export default HelloRecoil;
